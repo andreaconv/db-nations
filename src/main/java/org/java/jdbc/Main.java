2d6193efbdedfbccc00892fd85bfb1f6a07a405c
andreaconv <andrea2convertini@gmail.com>
2023-09-28 16:04:07 +0200

    Milestone 1

--- src/main/java/org/java/jdbc/Main.java
+++ src/main/java/org/java/jdbc/Main.java
@@ -2,6 +2,8 @@ package org.java.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Main {
 
@@ -11,16 +13,32 @@ public class Main {
 		final String user = "root";
 		final String password = "";
 		
-		try (Connection conn = DriverManager.getConnection(url, user, password)) {
+		try (Connection con = DriverManager.getConnection(url, user, password)) {
+
+			//inserire il nome del METODO
 			
-			System.out.println("Connessione stabilita correttamente");
+			readNations(con);
+			
 		} catch (Exception e) {
-			
 			System.out.println("Errore di connessione: " + e.getMessage());
 		}
-		
 		System.out.println("\n----------------------------------\n");
 		System.out.println("The end");
-		
 	}
+	
+	private static final void readNations(Connection con) {
+		
+		final String sql = " SELECT c.country_id, c.name, r.name, c2.name"
+				+ " FROM countries c "
+				+ " JOIN regions r "
+				+ " ON r.region_id = c.region_id"
+				+ " JOIN continents c2 "
+				+ " ON c2.continent_id = r.continent_id "
+				+ " ORDER BY c.name;";
+		
+		
+
+	}
+	
+	
 }
